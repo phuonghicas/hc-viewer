ab2a8fa249261816c4800adcb52cfe371f025274
phuonghicas <phuong_dt@ehicas.com.vn>
2026-03-04 14:12:41 +0700

    update new patch

---
+++ example/test-hc-viewer/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./public",
+    port: 5173,
+  },
+};
